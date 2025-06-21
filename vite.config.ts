f79d7b0f35446531f8fad9d2efdb0ad975d66d10
NAOYA804 <9494suruna@gmail.com>
2025-06-21 17:56:59 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  root: '.', // プロジェクトルート
+  publicDir: 'public', // 静的ファイル用フォルダ
   plugins: [react()],
   build: {
-    outDir: 'dist'
-  }
+    outDir: 'dist',
+  },
 })
