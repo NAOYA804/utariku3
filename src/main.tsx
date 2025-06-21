8f6ea892a9aa3ea08c75dfed14b32b17f9d4692d
NAOYA804 <9494suruna@gmail.com>
2025-06-21 17:33:10 +0900

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,1 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import SongRequestApp from './SongRequestApp';
 
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <SongRequestApp />
+  </React.StrictMode>
+);
