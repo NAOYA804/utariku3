ea93c689a38479ae9c964acc22a6f43ee7078024
NAOYA804 <9494suruna@gmail.com>
2025-06-22 09:48:46 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
-  root: '.',               // ルートはプロジェクト直下
-  publicDir: 'public',     // 静的ファイルは public
+  root: '.', // ルートはプロジェクト直下
+  publicDir: 'public', // 静的ファイルは public
   plugins: [react()],
   build: {
     rollupOptions: {
-      input: 'public/index.html', // ここで index.html の場所を指定！
+      input: path.resolve(__dirname, 'index.html'), // ✅ ルート直下の index.html を指定
     },
     outDir: 'dist',
   },
